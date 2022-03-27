6bae89d2047044ad163699d97da28a53489a7e4f
khj0426 <706shin1728@naver.com>
2022-03-27 15:19:04 +0900

    접두사

--- 접두사.cpp
+++ 접두사.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
-#include <queue> //이 헤더파일이 필요합니다
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 int main(void){
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    priority_queue<int> q;  //우선순위 큐의 선언 ->default로 큰 값이 우선순위가 크다.
-    
     int n;
     cin >> n;
+    
+    string arr[50];
     for(int i = 0;i<n;i++){
-        int element;
-        cin >> element;
-        if(element != 0){
-            q.push(element); //push연산
-        }
-        else if(element == 0){
-            if(q.empty()){ //q.empty() -> 큐가 비어있는지 여부
-                cout << '0' << '\n';
-            }
-            else{
-            cout << q.top() << '\n'; //top()->우선순위가 가장 큰 값 리턴
-            q.pop(); //pop연산
-            }
-        }
+        cin >> arr[i];
     }
+    sort(arr,arr+n);
+    int count = n;
+    
+       for(int i = 0;i<n;i++){
+       string temp = arr[i+1].substr(0,arr[i].size());
+       if(temp == arr[i]){
+          count--;
+       }
+    }
+    cout << count;
     
 }

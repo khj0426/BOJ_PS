0b076b8968af6ca32949da1a04d4b9d72c0f6403
khj0426 <706shin1728@naver.com>
2022-03-27 15:20:13 +0900

    수강신청

--- 수강신청.cpp
+++ 수강신청.cpp
@@ -2,7 +2,7 @@
 #include <map>
 #include <vector>
 #include <algorithm>
-//백준 13414 수강신청
+
 using namespace std;
 
 bool cmp(pair<string,int> a,pair<string,int> b){
@@ -21,18 +21,18 @@ int main(void){
         string num;
         cin >> num;
         iter = m.find(num);
-        if(iter != m.end()){ //이미 학번이 있는경우 i가 큰 순서로 갱신
+        if(iter != m.end()){ //이미 학번이 있는경우
             m[num] = i;
         }
-        else if(iter == m.end()){ //학번이 없는 경우 map에 넣기
+        else if(iter == m.end()){
             m.insert(pair<string,int>(num,i));
         }
     }
-    for(iter = m.begin();iter != m.end();iter++){ //map의 요소를 vector에 넣어두기
+    for(iter = m.begin();iter != m.end();iter++){
         v.push_back(pair<string,int>(iter->first,iter->second));
     }
     
-    sort(v.begin(),v.end(),cmp);//vector의 second기준으로 오름차순 정렬(i가 작은순서대로)
+    sort(v.begin(),v.end(),cmp);
     int count = 0;
     
     for(viter = v.begin();viter != v.end();viter++){

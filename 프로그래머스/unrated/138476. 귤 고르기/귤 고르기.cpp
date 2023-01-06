4b70697d99263ac78e4b6f2e488739a558db34c9
Kim0426 <706shin1728@naver.com>
2023-01-06 21:59:41 +0900

    [unrated] Title: 귤 고르기, Time: 32.39 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+//13254523
+//4개
+
+//1 1
+//2 2
+//3 2
+//4 1
+//5 2
+
+//5 3 2 1 4
+bool cmp(pair<int,int> a,pair<int,int> b){
+    if(a.second != b.second){
+        return a.second > b.second;
+    }
+    else{
+        return a.first < b.first;
+    }
+    
+}
+int solution(int k, vector<int> tangerine) {
+    map<int,int> countOfNumber;
+    map<int,int>::iterator iter;
+    
+    int answer = 0;
+    for(int i = 0;i<tangerine.size();i++){
+        iter = countOfNumber.find(tangerine[i]);
+        if(iter == countOfNumber.end()){
+            countOfNumber.insert({tangerine[i],1});
+        }
+        else{
+            countOfNumber[tangerine[i]]++;
+        }
+    
+    }
+    vector<pair<int,int>> v;
+    for(iter = countOfNumber.begin();iter != countOfNumber.end();iter++){
+       v.push_back({iter->first,iter->second});
+    }
+    
+    sort(v.begin(),v.end(),cmp);
+    for(int i = 0;i<v.size();i++){
+        k -= v[i].second;
+        answer += 1;
+        if(k <= 0){
+            break;
+        }
+     
+    }
+    
+    
+    
+    
+    
+    return answer;
+}
